ad8403ecc079fd6c70102a6c97e8e636a785eba7
Pranav Shivkumar <pshivkum@cisco.com>
2023-08-22 17:21:21 -0400

    fix refs in form

--- src/components/Contact/index.tsx
+++ src/components/Contact/index.tsx
@@ -1,46 +1,98 @@
-import React, { useRef, useEffect, useState } from 'react'
-import emailjs from '@emailjs/browser'
-import { Button, FormControl, FormLabel, Input, Textarea } from '@chakra-ui/react';
+import React, { useRef, useEffect, useState } from "react";
+import emailjs from "@emailjs/browser";
+import {
+  Button,
+  FormControl,
+  FormLabel,
+  Input,
+  Textarea,
+  VStack,
+} from "@chakra-ui/react";
+import { Field, Form, Formik } from "formik";
 
 const Contact = () => {
-    const emailRef = useRef<HTMLInputElement>(null);
-    const nameRef = useRef<HTMLInputElement>(null);
-    const [loading, setLoading] = useState<boolean>(false);
+  const emailRef = useRef<HTMLInputElement>(null);
+  const nameRef = useRef<HTMLInputElement>(null);
+  const [loading, setLoading] = useState<boolean>(false);
 
-    useEffect(() => emailjs.init("Olsev5DtTdwvneSxV"), [])
+  useEffect(() => emailjs.init("Olsev5DtTdwvneSxV"), []);
 
-    const handleSubmit = async (e) => {
-        e.preventDefault();
+  const handleSubmit = async () => {
+    const serviceId = "service_hzjdb2n";
+    const templateId = "contact_form";
 
-        const serviceId = "service_ux7anhh"
-        const templateId = "contact_form"
-
-        try {
-            setLoading(true)
-            await emailjs.send(serviceId, templateId, {
-                from_name: nameRef.current?.value, 
-                recipient: emailRef.current?.value 
-            });
-            alert("Email successfully sent!")
-        } catch(error) {
-            console.log(error)
-        }
-        finally {
-            setLoading(false)
-        }
+    try {
+      setLoading(true);
+      await emailjs.send(serviceId, templateId, {
+        from_name: nameRef.current?.value,
+        recipient: emailRef.current?.value,
+      });
+      alert("Email successfully sent!");
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setLoading(false);
     }
+  };
 
-    return (
-        <FormControl onSubmit={handleSubmit}>
-            <FormLabel>Name</FormLabel>
-            <Input ref={nameRef} type="text" name="user_name" />
-            <FormLabel>Email</FormLabel>
-            <Input ref={emailRef} type="email" name="user_email" />
-            <FormLabel>Message</FormLabel>
-            <Textarea name="message" />
-            <Button isLoading={loading}>Send!</Button>
-        </FormControl>
-    )
-}
+  console.log(nameRef.current?.value);
+
+  return (
+    <Formik
+      initialValues={{
+        userName: "",
+        userEmail: "",
+        message: "",
+      }}
+      onSubmit={handleSubmit}
+    >
+      {({ values, errors, touched, resetForm }) => (
+        <Form>
+          <VStack spacing={3}>
+            <FormControl>
+              <FormLabel htmlFor="userName">Name:</FormLabel>
+              <Field
+                innerRef={nameRef}
+                as={Input}
+                type="text"
+                name="userName"
+                id="userName"
+              />
+            </FormControl>
+
+            <FormControl>
+              <FormLabel htmlFor="userEmail">Email:</FormLabel>
+              <Field
+                innerRef={emailRef}
+                as={Input}
+                type="text"
+                name="userEmail"
+                id="userEmail"
+              />
+            </FormControl>
+
+            <FormControl>
+              <FormLabel htmlFor="message">Message:</FormLabel>
+              <Field as={Textarea} type="text" name="message" id="message" />
+            </FormControl>
+
+            <Button type="submit" isDisabled={loading}>
+              Submit
+            </Button>
+          </VStack>
+        </Form>
+      )}
+    </Formik>
+    // <FormControl onSubmit={handleSubmit}>
+    //     <FormLabel>Name</FormLabel>
+    //     <Input ref={nameRef} type="text" name="user_name" />
+    //     <FormLabel>Email</FormLabel>
+    //     <Input ref={emailRef} type="email" name="user_email" />
+    //     <FormLabel>Message</FormLabel>
+    //     <Textarea name="message" />
+    //     <Button isLoading={loading}>Send!</Button>
+    // </FormControl>
+  );
+};
 
 export default Contact;
