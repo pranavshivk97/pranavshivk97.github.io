3abfb7a05fe258a4af5b99a6427b93a285c62ec5
Pranav Shivkumar <pshivkum@cisco.com>
2023-09-22 19:15:44 -0400

    added about me page

--- src/routes/Home/index.tsx
+++ src/routes/Home/index.tsx
@@ -11,32 +11,34 @@ import {
 
 const Home = () => {
   return (
-      <Center height="800px" id="home-wrapper" w={[400, '100%']}>
-        <SimpleGrid minChildWidth="200px" spacing={3} m={10} p={10} background="blackAlpha.900">
-          <Box>
-            <Image
-              borderRadius="full"
-              boxSize="300px"
-              src="/pranav_shivkumar.JPG"
-              alt="Pranav Shivkumar"
-            />
-          </Box>
+    <Center height="800px" id="home-wrapper" w={[400, "100%"]}>
+      <SimpleGrid
+        minChildWidth="200px"
+        spacing={3}
+        m={10}
+        p={10}
+        background="blackAlpha.900"
+      >
+        <Box>
+          <Image
+            borderRadius="full"
+            boxSize="300px"
+            src="/pranav_shivkumar.JPG"
+            alt="Pranav Shivkumar"
+          />
+        </Box>
 
-          <Box>
-            <VStack align="left" spacing={3}>
-              <Text fontSize="lg">Hi, I am</Text>
-              <Heading as="h1" size="4xl">
-                Pranav Shivkumar
-              </Heading>
-              <Text fontSize="xl">
-                I am a Software Engineer & Full Stack Developer with over 2 years
-                of professional experience. I specialize in building full stack
-                web applications and I currently work at Cisco.
-              </Text>
-            </VStack>
-          </Box>
-        </SimpleGrid>
-      </Center>
+        <Box>
+          <VStack align="left" spacing={3}>
+            <Text fontSize="lg">Hi, I am</Text>
+            <Heading as="h1" size="4xl">
+              Pranav Shivkumar
+            </Heading>
+            <Text fontSize="xl">Software Engineer @ Cisco Systems</Text>
+          </VStack>
+        </Box>
+      </SimpleGrid>
+    </Center>
   );
 };
 
